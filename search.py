9395021c0b4181873d43d6f91107d04c66e212ea
Madeline Mitchell <mitchie0407@gmail.com>
2024-07-04 20:30:54 -0400

    Adding additional specific search functions

--- search.py
+++ search.py
@@ -1,20 +1,22 @@
 from spotipy.oauth2 import SpotifyClientCredentials
 from pandas import read_csv
 import spotipy
-import sys
 import pprint
 
 keys = read_csv("keys.csv",index_col="names")
-
-if len(sys.argv) > 1:
-    search_str = sys.argv[1]
-else:
-    search_str = 'Kid Cudi'
-
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=keys.at["client_id","info"],
                                                                          client_secret=keys.at["client_secret","info"]))
-result = sp.search('artist:' + search_str,type="artist")
-items = result['artists']['items']
-if len(items) > 0:
-    artist = items[0]
-    pprint.pprint(artist)
+
+def artist_search(artist="Kid Cudi",sp=sp):
+    result = sp.search('artist:' + artist,type="artist")
+    items = result['artists']['items']
+    if len(items) > 0:
+        artist_object = items[0]
+    return artist_object
+
+def song_search(song="What Makes You Beautiful",sp=sp):
+    result = sp.search('song:' + song,type="song")
+    items = result['songs']['items']
+    if len(items) > 0:
+        song_object = items[0]
+    return song_object
