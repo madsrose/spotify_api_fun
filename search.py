06fbc79b4e334f5f64a2b0f3a54e763c04584271
Madeline Mitchell <mitchie0407@gmail.com>
2024-07-05 19:01:44 -0400

    Adding additional search functions

--- search.py
+++ search.py
@@ -24,5 +24,34 @@ def song_search(song="the other side of the door",artist="NA",sp=sp):
         song_object = items[0]
     return song_object
 
+def album_search(album="Rumours",artist="NA",sp=sp):
+    p = 'album:' + album
+    if artist != "NA":
+        p = p + ' artist:' + artist 
+    result = sp.search(q=p ,type="album")
+    items = result['albums']['items']
+    if len(items) > 0:
+        album_object = items[0]
+    return album_object
 
-pprint.pprint(song_search("happier than ever"))
+def get_artist_uri(artist="Kid Cudi",sp=sp):
+    artist_object = artist_search(artist=artist,sp=sp)
+    return artist_object["uri"]
+
+
+def get_song_uri(song="the other side of the door",artist="NA",sp=sp):
+    song_object = song_search(song=song,artist=artist)
+    return song_object["uri"]
+
+def get_album_uri(album="Rumours",artist="NA",sp=sp):
+    album_object = album_search(album=album,artist=artist)
+    return album_object["uri"]
+
+
+def get_artist_albums(artist="Kid Cudi",sp=sp):
+    results = sp.artist_albums(get_artist_uri(artist), album_type='album')
+    albums = results['items']
+    while results['next']:
+        results = sp.next(results)
+        albums.extend(results['items'])
+    return albums
